82d539eb99055d212e1e4781a4715b4c1313917e
iamjaylee <jay.lee@waggl.com>
2019-02-28 13:00:12 -0600

    Change devise paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { 
     omniauth_callbacks: "users/omniauth_callbacks"
-  }
+  }, skip: [:sessions]
+
+  as :user do
+    get "login", to: "devise/sessions#new", as: :new_user_session
+    post "login", to: "devise/sessions#create", as: :user_session
+    delete "logout", to: "devise/sessions#destroy", as: :destroy_user_session
+  end
 
   root "calendars#index"
   resources :calendars
